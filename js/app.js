403123e136eeb50e585001510ae5f1b72bb3267f
bluesatch <satchwerk@gmail.com>
2024-12-05 10:40:09 -0600

    first pass

--- js/app.js
+++ js/app.js
@@ -25,17 +25,25 @@ class Game {
         this.speed = 100
 
         this.changingDirection = false 
+
+        this.foodX = 0
+        this.foodY = 0
     }
 
 
     init() {
         
+        if (this.hasGameEnded()) {
+            return 
+        }
+
+        this.changingDirection = false 
         // make a timer
         /** setTimeout(callback function, time in ms) */
         setTimeout(()=> {
             this.makeCanvas()
             this.drawSnake()
-            
+            this.drawFood()
             this.moveSnake()
 
             // call init(); recursion
@@ -44,46 +52,7 @@ class Game {
         }, this.speed)
     }
 
-    // 1 make canvas
-    makeCanvas() {
-        const snakeBoard = this.snakeBoard
-        const snakeBoardCtx = this.snakeBoardCtx
-        snakeBoardCtx.fillStyle = this.boardBg
-        snakeBoardCtx.strokeStyle = this.boardBorder
-        snakeBoardCtx.fillRect(0, 0, snakeBoard.width, snakeBoard.height)
-        snakeBoardCtx.strokeRect(0, 0, snakeBoard.width, snakeBoard.height)
-    }
-
-    // 2 draw snake 
-    drawSnake() {
-
-        const snake = this.snake 
-        const snakeBoardCtx = this.snakeBoardCtx
-
-        snake.forEach(snakePart => {
-            snakeBoardCtx.fillStyle = this.snakeColor
-            snakeBoardCtx.strokeStyle = this.snakeBorder
-
-            snakeBoardCtx.fillRect(snakePart.x, snakePart.y, 10, 10)
-            snakeBoardCtx.strokeRect(snakePart.x, snakePart.y, 10, 10)
-        })
-    }
-
-    // 3 move snake
-    moveSnake() {
-        const snake = this.snake
-        const head = {
-            x: snake[0].x + this.dx,
-            y: snake[0].y + this.dy
-        }
-
-        snake.unshift(head)
-
-        snake.pop()
-    }
-
-
-    // change direction 
+    // 4 change direction 
     changeDirection(e) {
 
         const LEFT = 37
@@ -121,6 +90,98 @@ class Game {
             this.dy = 10
         }
     }
+
+    // 5 draw food
+    drawFood() {
+        this.snakeBoardCtx.fillStyle = 'darkgoldenrod'
+        this.snakeBoardCtx.strokeStyle = 'limegreen'
+        this.snakeBoardCtx.fillRect(this.foodX, this.foodY, 10 , 10)
+        this.snakeBoardCtx.strokeRect(this.foodX, this.foodY, 10, 10)
+    }
+
+    randomFood(min, max) {
+        return Math.round((Math.random() * (max - min) + min) / 10) * 10
+    }
+
+    // 2 draw snake 
+    drawSnake() {
+
+        const snake = this.snake 
+        const snakeBoardCtx = this.snakeBoardCtx
+
+        snake.forEach(snakePart => {
+            snakeBoardCtx.fillStyle = this.snakeColor
+            snakeBoardCtx.strokeStyle = this.snakeBorder
+
+            snakeBoardCtx.fillRect(snakePart.x, snakePart.y, 10, 10)
+            snakeBoardCtx.strokeRect(snakePart.x, snakePart.y, 10, 10)
+        })
+    }
+
+    // 6 generate food
+    generateFood() {
+        this.foodX = this.randomFood(0, this.snakeBoard.width - 10)
+        this.foodY = this.randomFood(0, this.snakeBoard.height - 10)
+
+        this.snake.forEach(part => {
+            // collision 
+            const hasEaten = part.x === this.foodX && part.y === this.foodY
+
+            if (hasEaten) {
+                this.generateFood()
+            }
+        })
+    }
+
+    hasGameEnded() {
+        const snake = this.snake
+        const snakeBoard = this.snakeBoard 
+
+        for (let i = 4; i < snake.length; i++) {
+            if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+        }
+
+        const hitLeftWall = snake[0].x < 0
+        const hitRightWall = snake[0].x > snakeBoard.width - 10
+        const hitTopWall = snake[0].y < 0
+        const hitBottomWall = snake[0].y > snakeBoard.height - 10
+
+        return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
+
+    }
+
+    // 1 make canvas
+    makeCanvas() {
+        const snakeBoard = this.snakeBoard
+        const snakeBoardCtx = this.snakeBoardCtx
+        snakeBoardCtx.fillStyle = this.boardBg
+        snakeBoardCtx.strokeStyle = this.boardBorder
+        snakeBoardCtx.fillRect(0, 0, snakeBoard.width, snakeBoard.height)
+        snakeBoardCtx.strokeRect(0, 0, snakeBoard.width, snakeBoard.height)
+    }
+
+    // 3 move snake
+    moveSnake() {
+        const snake = this.snake
+        const head = {
+            x: snake[0].x + this.dx,
+            y: snake[0].y + this.dy
+        }
+
+        snake.unshift(head)
+
+        // 7 when snake eats food 
+        const hasEatenFood = snake[0].x === this.foodX && snake[0].y === this.foodY
+
+        if (hasEatenFood) {
+            this.generateFood()
+        } else {
+            snake.pop()
+        }
+
+    }
+
+
 }
 
 const snake = new Game() 
